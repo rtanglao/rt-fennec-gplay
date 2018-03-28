e7ae597ff3acbce9ff72249c57b7a835573424a8
Roland Tanglao <roland@rolandtanglao.com>
2018-03-27 22:23:17 -0700

    change from utf16 to utf8

--- read-reviews-replies.rb
+++ read-reviews-replies.rb
@@ -42,6 +42,7 @@ reviewsColl = db[:reviews]
   # last field has a "\n" so use chomp to eliminate it?
 # CSV.foreach(ARGV[0]) do |row|
 ARGF.lines do |row|
+        row = row.encode('utf-8') 
   PP::pp(row, $stderr)
         exit
 end 
