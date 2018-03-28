e2408f5683754702d2b6ce2f61237b8ff13604ba
Roland Tanglao <roland@rolandtanglao.com>
2018-03-27 22:34:31 -0700

    Update read-reviews-replies.rb

--- read-reviews-replies.rb
+++ read-reviews-replies.rb
@@ -42,6 +42,7 @@ reviewsColl = db[:reviews]
   # last field has a "\n" so use chomp to eliminate it?
 # CSV.foreach(ARGV[0]) do |row|
 ARGF.lines do |row|
+        row = row.chomp
           PP::pp(row, $stderr)     
        # row = row.chomp.encode("UTF-8", "UTF-16")
   #PP::pp(row, $stderr)
