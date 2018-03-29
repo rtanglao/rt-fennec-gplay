362dc19ea6b64200b6747a1e7c98fea8ad2563d7
Roland Tanglao <roland@rolandtanglao.com>
2018-03-29 13:33:14 -0700

    Update read-reviews-replies.rb

--- read-reviews-replies.rb
+++ read-reviews-replies.rb
@@ -41,7 +41,8 @@ reviewsColl.indexes.create_one({ "id" => -1 }, :unique => true)
 CSV.open(ARGV[0], 'rb:bom|utf-16', :headers => true) do |csv|
   csv.each do |row| 
     logger.debug row.ai 
-    row["review_submitted_time"] = Time.at((row["Review Submit Millis Since Epoch"].to_i)/1000).utc
+        t = row["Review Submit Millis Since Epoch"].to_i/1000
+    row["review_submitted_time"] = Time.at(t.utc)
     logger.debug row["Review Submit Millis Since Epoch"].ai
     logger.debug row["review_submitted_time"].ai
     pp row
