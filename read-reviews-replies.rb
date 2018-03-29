eb89239d693646bf3b516819a46da2a222382f82
Roland Tanglao <roland@rolandtanglao.com>
2018-03-29 15:17:38 -0700

    Update read-reviews-replies.rb

--- read-reviews-replies.rb
+++ read-reviews-replies.rb
@@ -43,11 +43,8 @@ CSV.open(ARGV[0], :headers => true) do |csv|
   csv.each do |row| 
         r1 = Hash(row)
     logger.debug r1.ai 
-        puts r1["Review Submit Millis Since Epoch".to_sym]
-        str="Review Submit Millis Since Epoch".force_encoding("utf-16")
-        pp r1[str]
         pp r1
-        pp r1[:"Review Link"]
+        pp r1["Review Link"]
         pp r1["Device"]
         puts "x"
         x = {}
