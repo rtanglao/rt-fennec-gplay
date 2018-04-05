e73354c9b414392eeaf8213329aaebd10ac16ec0
Roland Tanglao <roland@rolandtanglao.com>
2018-04-04 20:12:22 -0700

    Update read-reviews-replies.rb

--- read-reviews-replies.rb
+++ read-reviews-replies.rb
@@ -68,13 +68,10 @@ CSV.open(ARGV[0], :headers => true) do |rating_review_data|
     logger.debug r1["firefox_major_version"].ai
     r1["star_rating"] = r1["Star Rating"].to_i
     logger.debug r1["star_rating"].ai
-      next if r1["Review Link"].nil?
-    template = Addressable::Template.new("https://{host}{/segments*}/{?account,reviewid}"
-)
-uri = Addressable::URI.parse(
-  r1["Review Link"]
-)
-ap template.extract(uri)
+    next if r1["Review Link"].nil?
+    template = Addressable::Template.new("https://{host}{/segments*}/{?account,reviewid}")
+    uri = Addressable::URI.parse(r1["Review Link"])
+    ap template.extract(uri)
    # uri = Addressable::URI.parse(r1["Review Link"])
     #awesome_print uri.query_values
   end
