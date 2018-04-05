4da2e8cc07f52a0d039bcd8038ccc4c4ee452c69
Roland Tanglao <roland@rolandtanglao.com>
2018-04-04 21:03:26 -0700

    comment out debug prints

--- read-reviews-replies.rb
+++ read-reviews-replies.rb
@@ -52,27 +52,27 @@ review_id_key = '&reviewid='
 CSV.open(ARGV[0], :headers => true) do |rating_review_data|      
   rating_review_data.each do |rating_review| 
     r1 = Hash(rating_review)
-    logger.debug r1.ai 
+    #logger.debug r1.ai 
     r1["review_submitted_time"] = calc_mongo_time_from_string_milliseconds(r1["Review Submit Millis Since Epoch"])
     r1["review_last_updated_time"] = calc_mongo_time_from_string_milliseconds(r1["Review Last Update Millis Since Epoch"])   
     r1["developer_last_updated_time"] = calc_mongo_time_from_string_milliseconds(r1["Developer Reply Millis Since Epoch"])
-    logger.debug r1["developer_last_updated_time"].ai    
-    logger.debug r1["review_submitted_time"].ai
-    logger.debug r1["review_last_updated_time"].ai
+    #logger.debug r1["developer_last_updated_time"].ai    
+    #logger.debug r1["review_submitted_time"].ai
+    #logger.debug r1["review_last_updated_time"].ai
     next if r1["App Version Name"].nil?
     firefox_version_array = r1["App Version Name"].split('.')
     if !firefox_version_array.nil?
-      logger.debug firefox_version_array[0].ai
+      #logger.debug firefox_version_array[0].ai
     end
     firefox_version = firefox_version_array[0]
     r1["firefox_major_version"] = firefox_version.to_i if  !firefox_version.nil?
-    logger.debug r1["firefox_major_version"].ai
+    #logger.debug r1["firefox_major_version"].ai
     r1["star_rating"] = r1["Star Rating"].to_i
-    logger.debug r1["star_rating"].ai
+    #logger.debug r1["star_rating"].ai
     review_link = r1["Review Link"]
     next if review_link.nil?
     index = review_link.index(review_id_key)
-    r1["id"] = ap review_link[index + review_id_key.length..-1].chomp
-    logger.debug r1["id"]
+    #r1["id"] = ap review_link[index + review_id_key.length..-1].chomp
+    logger.debug r1.ai
   end
 end
