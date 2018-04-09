a3d54eb42caf62774bbf2fd44df099a932181fd9
Roland Tanglao <roland@rolandtanglao.com>
2018-04-09 12:04:07 -0700

    Update synthetic_add_device_branding_marketing_model.rb

--- synthetic_add_device_branding_marketing_model.rb
+++ synthetic_add_device_branding_marketing_model.rb
@@ -47,8 +47,9 @@ CSV.open(ARGV[0], :headers => true) do |device_branding_marketing_model_data|
 
     d1["id"] = Digest::SHA2.new(256).hexdigest(id_hashstr)
     d1["synthetic_branding_plus_marketing_name"] = ""
-    d1["synthetic_branding_plus_marketing_name"] = d1["Retail Branding"] + d1["Marketing Name"]\
-      if !d1["Retail Branding"].nil? || !d1["Marketing Name"].nil?
+    d1["synthetic_branding_plus_marketing_name"] += d1["Retail Branding"] if !d1["Retail Branding"].nil?
+    d1["synthetic_branding_plus_marketing_name"] += d1["Marketing Name"] if !d1["Marketing Name"].nil?
+
     logger.debug d1.ai
     logger.debug devicesColl.find({ 'id' => d1["id"] }).update_one(d1, :upsert => true ).ai
   end
