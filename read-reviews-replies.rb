0c2f83cc2486828f4356da1b23e8adc10e6fb4cd
Roland Tanglao <roland@rolandtanglao.com>
2018-04-17 10:22:46 -0700

    Update read-reviews-replies.rb

--- read-reviews-replies.rb
+++ read-reviews-replies.rb
@@ -100,7 +100,7 @@ CSV.open(ARGV[0], :headers => true) do |rating_review_data|
     logger.debug r1.ai
     result = reviewsColl.find({ 'id' => r1["id"] }).update_one(r1, :upsert => true ).ai
     #result = Hash(result)
-    logger.debug "nModified " + result[0]["nModified"]
+    logger.debug "nModified " + result[1]["nModified"]
     logger.debug result.ai
     exit
   end
