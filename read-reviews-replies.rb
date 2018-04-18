8949d7fdb723a2b4753e64c7d909a79121180475
Roland Tanglao <roland@rolandtanglao.com>
2018-04-17 23:49:22 -0700

    Update read-reviews-replies.rb

--- read-reviews-replies.rb
+++ read-reviews-replies.rb
@@ -93,7 +93,7 @@ CSV.open(ARGV[0], :headers => true) do |rating_review_data|
     logger.debug r1.ai
     result_array = reviewsColl.find({ 'id' => r1["id"] }).update_one(r1, :upsert => true ).to_a
     ap result_array
-    nModified = result_array.each {|item| item.include?(:nModified)}
+    nModified = result_array.each {|item| item.include?("nModified")}
     #puts result[""]["nModified"]
     ap nModified
     exit
